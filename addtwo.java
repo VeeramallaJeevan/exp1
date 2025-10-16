f341be45d7574a170cfcfaa7f4aaf064881a6e00
jeevan <v.jeevan.2005@gmail.com>
2025-10-16 15:23:49 +0530

    Update addtwo.java

--- addtwo.java
+++ addtwo.java
@@ -3,6 +3,6 @@ public class addtwo{
         int a = 10 ;
         int b = 20;
         int c = a + b;
-        System.out.print("the sum :",c):
+        System.out.print("the sum :",c);
           }
 }
